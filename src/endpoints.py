043362217728196369b205396cf16e7e2b58cad7
FatSoggy <vraj1228@comcast.net>
2025-11-20 21:41:24 -0500

    implemented auth_refresh endpoint, wip still broken?

--- src/endpoints.py
+++ src/endpoints.py
@@ -2,9 +2,11 @@ import requests
 import json
 from urllib.parse import urljoin 
 
+BASE_URL = 'https://api-ss-sandbox.betprophet.co' 
+
 def get_sporting_events(game_id: int,access_token: str):
-    BASE_URL = 'https://api-ss-sandbox.betprophet.co' 
-    ENDPOINT = '/partner/mm/get_sport_events' 
+
+    ENDPOINT = '/partner/mm/get_sport_events'
 
     full_url = urljoin(BASE_URL, ENDPOINT)
 
@@ -19,8 +21,32 @@ def get_sporting_events(game_id: int,access_token: str):
     response = requests.get(full_url, params=query_params, headers=headers)
 
     if response.status_code == 200:
-        print("Success! Sport Events:")
+        print('Success! Sport Events:')
         return response.json()
     else:
-        print(f"Failed: Status Code {response.status_code}")
+        print(f'Failed: Status Code {response.status_code}')
         print(response.text)
+
+def auth_refresh(refresh_token: str):
+    
+    ENDPOINT = '/partner/auth/refresh'
+
+    full_url = urljoin(BASE_URL, ENDPOINT)
+
+    headers = {
+        "Content-Type": "application/json",
+        "accept": "application/json"
+    }
+
+    body = {
+        "refresh_token": refresh_token
+    }
+    
+    response = requests.post(full_url, headers=headers, json=body)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Auth refresh failed: {response.status_code}")
+        print(response.text)
+        return None
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+import time
 import argparse
 import requests
 import json
 from urllib.parse import urljoin 
-from src.endpoints import get_sporting_events
+from src.endpoints import get_sporting_events, auth_refresh
 
 from src import mm_calls
 from src.log import logging
@@ -13,17 +14,28 @@ if __name__ == '__main__':
     mm_instance = mm_calls.MMInteractions()
     mm_instance.mm_login()
     mm_instance.get_balance()
+
+    NBA_ID = 132
     
     access_token = mm_instance.mm_session["access_token"]
+    refresh_token = mm_instance.mm_session["refresh_token"]
 
-    sporting_events = get_sporting_events(132, access_token)
+    '''
+    sporting_events = get_sporting_events(NBA_ID, access_token)
 
     if sporting_events:
         
         print(json.dumps(sporting_events, indent = 4))  
     else:
         logging.error("Failed to fetch sporting events")
+    '''
     
-    all_nba_games = []
+    
 
-    
+    refresh_response = auth_refresh(refresh_token)
+
+    if refresh_response:
+        access_token = refresh_response["data"]["access_token"]
+        print("Access token refreshed!")
+
+    print(mm_instance.mm_session["access_token"])
